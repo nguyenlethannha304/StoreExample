710d668c844a83807b4ad6c770b51f9b4bcde195
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-04-12 03:35:09 +0700

    Add random rating to product

--- backend/apps/products/baker_recipes.py
+++ backend/apps/products/baker_recipes.py
@@ -1,6 +1,14 @@
 from itertools import cycle
 from model_bakery.recipe import Recipe, foreign_key, related
 from .models import Category, Type, Product
+import random
+
+
+def return_random_rating():
+    random_number = random.randrange(0, 5)
+    return round(random_number, 1)
+
+
 # Category
 furniture_category = Recipe(Category, name='furniture')
 # Type
@@ -9,7 +17,7 @@ table_type = Recipe(Type, name='table')
 # Product
 chair_name_list = ['modern chair', 'wood chair', 'plastic chair']
 chair_products = Recipe(Product, name=cycle(
-    chair_name_list), type=foreign_key(chair_type))
+    chair_name_list), rating=return_random_rating, type=foreign_key(chair_type))
 table_name_list = ['modern table', 'wood table', 'plastic table']
 table_products = Recipe(Product, name=cycle(
-    table_name_list), type=foreign_key(table_type))
+    table_name_list), rating=return_random_rating, type=foreign_key(table_type))
