90ae3a1f038d7207eba00b6f57401841536d49fd
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-04-13 00:33:57 +0700

    Create data test for similar_product

--- frontend/src/app/data-for-test.ts
+++ frontend/src/app/data-for-test.ts
@@ -132,3 +132,20 @@ export const productDetail: ProductDetail = {
     'ipERmngwytfcgMAtwGGncTPVlkwMuzBfIkNWsqYmMyUXmaBXvsfYzYUuAUrKravzyaUFwXIrXLohfTpYAqWbwdGnmPRVFHvqxJgwhpXrZzBQztgBnenWGLkCJPFiHBVWCmwyCYVExzYYyUTmXLxFqDGeKFhcYsOhkTCyvmGZOQsEypZAYHFuWwAeFaZgktTpeuCLfiyBEoKxqueAUUysNcbQTOQDwSYtmVtZFnWrcjVOiyljGWSjRsaqwjqJelwElXWcStvwaKqRMKjFOQLgbHNOnIjGtyQlsGuRYwoBFSMs',
   type: 2,
 };
+export const similarProduct: ProductList['results'] = [
+  {
+    id: 'bfaab8db-d97b-4582-8f18-c8afd5417513',
+    price: 92648695,
+    thumbnail: null,
+  },
+  {
+    id: '2f564397-eaa5-4437-a3e9-45948332f24d',
+    price: 875324594,
+    thumbnail: null,
+  },
+  {
+    id: '9013c3ee-583f-47af-b2d4-6489fd5bade7',
+    price: 212558569,
+    thumbnail: null,
+  },
+];
