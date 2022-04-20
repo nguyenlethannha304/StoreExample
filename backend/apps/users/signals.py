b783e564088a9c292f0696757a38abec23fdb4d6
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-04-21 04:14:05 +0700

    Create cart when create user

--- backend/apps/users/signals.py
+++ backend/apps/users/signals.py
@@ -1,11 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Address
+from .models import Address
+from django.contrib.auth import get_user_model
+from django.apps import apps
+UserModel = get_user_model()
+Cart = apps.get_model('orders', 'Cart')
 
 
-@receiver(post_save, sender=CustomUser)
+@receiver(post_save, sender=UserModel)
 def create_adress_for_new_user(sender, instance, created, **kwargs):
     '''Create empty address object for new users'''
     if created is True:
         address = Address(user=instance, default_address=True)
         address.save()
+
+
+@receiver(post_save, sender=UserModel)
+def create_cart_for_new_user(sender, instance, created, **kwargs):
+    if created is True:
+        cart = Cart(user=instance)
+        cart.save()
