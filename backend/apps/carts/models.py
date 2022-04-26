a722cbad4cfe2cf6731b0328e34457b784d4ab5b
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-04-27 04:22:04 +0700

    Move orders Model

--- backend/apps/carts/models.py
+++ backend/apps/carts/models.py
@@ -1,11 +1,9 @@
-from django.utils.functional import cached_property
 from secrets import choice
-from django.db import IntegrityError, models, transaction
+from django.db import IntegrityError, models
 from django.db.models import F, Q
 from django.core.validators import MinValueValidator
 from django.contrib.auth import get_user_model
 import uuid
-from django.core.serializers.json import DjangoJSONEncoder
 UserModel = get_user_model()
 # Create your models here.
 
@@ -67,68 +65,3 @@ class CartItem(models.Model):
     def update_cart_item(cls, cart_id, product_id, quantity):
         cls.objects.filter(cart_id=cart_id, product=product_id, quantity=quantity).update(
             quantity=F('quantity') + quantity)
-
-
-class Order(models.Model):
-    ORDER_STATUS = [
-        ('P', 'Pending'),   # After submit
-    ]
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    status = models.CharField(max_length=1, choices=ORDER_STATUS)
-    user = models.ForeignKey(
-        UserModel, on_delete=models.SET_NULL, null=True)
-    item_prices = models.PositiveBigIntegerField(null=True)
-    shipping_fee = models.PositiveBigIntegerField(null=True)
-    total_price = models.PositiveBigIntegerField(null=True)
-    address = models.ForeignKey(
-        'users.Address', on_delete=models.SET_NULL, null=True)
-    created_date = models.DateTimeField()
-    completed_date = models.DateTimeField(null=True)
-    number_of_item = models.IntegerField(default=0)
-
-    @ cached_property
-    def items_of_order(self):
-        return ItemOrder.objects.filter(order=self.id).select_related('product')
-
-    @ cached_property
-    def caculate_item_prices(self):
-        self.item_prices = 0
-        for item in self.items_of_order:
-            self.item_prices += item.caculate_price()
-        return self.item_prices
-
-    @ cached_property
-    def caculate_shipping_fee(self):
-        self.shipping_fee = 0
-        for item in self.items_of_order:
-            self.shipping_fee += item.caculate_shipping_fee()
-        return self.shipping_fee
-
-    def caculate_total_order_price(self):
-        return self.caculate_item_prices + self.caculate_shipping_fee
-
-    def update_item_to_db(self):
-        ItemOrder.objects.bulk_update(
-            self.items_of_order, ['price', 'shipping_fee'])
-
-    def save_order_to_db(self, *args, **kwargs):
-        self.update_item_to_db()
-        return super().save(*args, **kwargs)
-
-
-class ItemOrder(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    order = models.ForeignKey(
-        Order, related_name='items', on_delete=models.SET_NULL, null=True)
-    products = models.ForeignKey(
-        "products.Product", on_delete=models.SET_NULL, null=True, related_name='items')
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.PositiveBigIntegerField(null=True)
-    shipping_fee = models.PositiveBigIntegerField(default=0)
-
-    def caculate_price(self):
-        self.price = self.product.price * self.quantity
-        return self.price
-
-    def caculate_shipping_fee(self):
-        return 0
