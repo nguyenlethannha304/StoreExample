e6bab35d97f1e2c73cd741d36f43f98e3f7aa1fa
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-06-20 20:19:58 +0700

    Code similar product

--- frontend/src/app/products/product-detail/product-detail.component.ts
+++ frontend/src/app/products/product-detail/product-detail.component.ts
@@ -16,38 +16,7 @@ export class ProductDetailComponent implements OnInit {
   ) {}
   uuid: ProductDetail['id'];
   product: ProductDetail;
-  similarProducts: ProductCard[] = [
-    {
-      id: 'bfaab8db-d97b-4582-8f18-c8afd5417513',
-      price: 92648695,
-      thumbnail: null,
-    },
-    {
-      id: '2f564397-eaa5-4437-a3e9-45948332f24d',
-      price: 875324594,
-      thumbnail: null,
-    },
-    {
-      id: '9013c3ee-583f-47af-b2d4-6489fd5bade7',
-      price: 212558569,
-      thumbnail: null,
-    },
-    {
-      id: 'bfaab8db-d97b-4582-8f18-c8afd5417513',
-      price: 92648695,
-      thumbnail: null,
-    },
-    {
-      id: '2f564397-eaa5-4437-a3e9-45948332f24d',
-      price: 875324594,
-      thumbnail: null,
-    },
-    {
-      id: '9013c3ee-583f-47af-b2d4-6489fd5bade7',
-      price: 212558569,
-      thumbnail: null,
-    },
-  ];
+  similarProducts: ProductCard[] = null;
   ngOnInit(): void {
     this.route.params.subscribe((value) =>
       this.getProductDetail(value['uuid'])
@@ -56,6 +25,14 @@ export class ProductDetailComponent implements OnInit {
   getProductDetail(uuid: ProductDetail['id']) {
     this.productService.getProductDetail(uuid).subscribe((responseBody) => {
       this.product = responseBody;
+      this.getSimilarProduct(this.product.type, this.product.id);
     });
+  }
+  getSimilarProduct(type_id: number, exclude_product_id: string) {
+    this.productService
+      .getSimilarProducts(type_id, exclude_product_id)
+      .subscribe((products) => {
+        this.similarProducts = products;
+      });
   }
 }
--- frontend/src/app/products/shared/service/product.service.ts
+++ frontend/src/app/products/shared/service/product.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProductDetail, ProductCardList } from '../interface/products';
+import {
+  ProductDetail,
+  ProductCard,
+  ProductCardList,
+} from '../interface/products';
 import { environment as e } from 'src/environments/environment';
 import { UUID } from 'src/app/shared/interface/share';
 @Injectable({
@@ -24,5 +28,10 @@ export class ProductService {
   getProductDetail(uuid: UUID) {
     // Get a specific products
     return this.httpClient.get<ProductDetail>(`${e.api}/products/${uuid}/`);
+  }
+  getSimilarProducts(type_id: number, exclude_product_id: string) {
+    return this.httpClient.get<ProductCard[]>(
+      `${e.api}/products/similar_product/${type_id}/${exclude_product_id}/`
+    );
   }
 }
