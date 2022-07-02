d31f651d0d75ba743fc1ce0bff41f3a73a3fcd38
Nha Nuyen <nha@Nhas-Mac-mini.local>
2022-07-02 08:24:50 +0700

    Add nginx and angular production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://127.0.0.1:8000/api',
+  django: 'http://192.168.0.180',
+  api: 'http://192.168.0.180/api',
   accessTokenExpireIn: 5 * 60 * 1000, // 5 mins
   refreshTokenExpireIn: 1 * 24 * 60 * 60 * 1000, // 1 days
 };
