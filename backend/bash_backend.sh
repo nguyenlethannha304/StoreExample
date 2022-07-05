c09f9c3f135fa46ad786fd45571c2b4be7f5bfc9
Nha <nguyenlethanhnha304@gmail.com>
2022-07-05 18:00:06 +0700

    Code bash script for frontend and backend

--- backend/bash_backend.sh
+++ backend/bash_backend.sh
@@ -5,7 +5,7 @@ export $(sed '/^\#/d' ../env/django_env)
 export $(sed '/^\#/d' ../env/post_env)
 
 # Export static for admin page
-python ./manage.py collectstatic
+python ./manage.py collectstatic --noinput
 
 # Run server
 gunicorn settings.wsgi --bind 0.0.0.0:5000
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  django: 'http://192.168.0.180',
-  api: 'http://192.168.0.180/api',
+  django: 'http://0.0.0.0',
+  api: 'http://0.0.0.0/api',
   accessTokenExpireIn: 5 * 60 * 1000, // 5 mins
   refreshTokenExpireIn: 1 * 24 * 60 * 60 * 1000, // 1 days
 };
