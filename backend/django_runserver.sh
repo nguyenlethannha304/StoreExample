9a4cc8132005050ca1ba9e7c1f14d80c3c9fe6bb
Nha <nguyenlethanhnha304@gmail.com>
2022-07-05 07:31:43 +0700

    Set up for django static

--- backend/django_runserver.sh
+++ backend/django_runserver.sh
@@ -4,4 +4,8 @@
 export $(sed '/^\#/d' ../env/django_env)
 export $(sed '/^\#/d' ../env/post_env)
 
+# Export static for admin page
+python ./manage.py collectstatic
+
+# Run server
 gunicorn settings.wsgi --bind 0.0.0.0:5000
