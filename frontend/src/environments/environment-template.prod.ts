235d68181fa62d4c037e3b35a4ce6635e776ffd0
Nha <nguyenlethanhnha304@gmail.com>
2022-07-17 03:53:54 +0700

    Use envsub package to replace shell variable to environment

---
+++ frontend/src/environments/environment-template.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    django: 'http://${PUBLIC_IP}',
+    api: 'http://${PUBLIC_IP}/api',
+    accessTokenExpireIn: 5 * 60 * 1000, // 5 mins
+    refreshTokenExpireIn: 1 * 24 * 60 * 60 * 1000, // 1 days
+  };
+  
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  production: true,
-  django: 'http://0.0.0.0',
-  api: 'http://0.0.0.0/api',
-  accessTokenExpireIn: 5 * 60 * 1000, // 5 mins
-  refreshTokenExpireIn: 1 * 24 * 60 * 60 * 1000, // 1 days
-};
+  // Get from environment-template-prod
+  };
+  
